51649de2769f834dad80506cb1c4d505b5d3dc18
Ryan Kienstra <ryan.kienstra@wpengine.com>
2024-06-03 08:53:38 -0600

    Add a README file

--- task/task.go
+++ task/task.go
@@ -80,6 +80,28 @@ type TaskEvent struct {
 	Task      Task
 }
 
+var stateTransitionMap = map[State][]State{
+	Pending:   []State{Scheduled},
+	Scheduled: []State{Scheduled, Running, Failed},
+	Running:   []State{Running, Completed, Failed},
+	Completed: []State{},
+	Failed:    []State{},
+}
+
+func Contains(states []State, state State) bool {
+	for _, s := range states {
+		if s == state {
+			return true
+		}
+	}
+
+	return false
+}
+
+func ValidStateTransition(src State, dst State) bool {
+	return Contains(stateTransitionMap[src], dst)
+}
+
 func (d *Docker) Run() DockerResult {
 	ctx := context.Background()
 	reader, err := d.Client.ImagePull(
